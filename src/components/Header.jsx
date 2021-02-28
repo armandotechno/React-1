23a082e5ae6abba8d93647d12b087a7cf66e023f
Armando <armandotechno12@gmail.com>
2021-02-27 22:04:42 -0400

    Agregando el Footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     
     return(
-        <h1 id="encabezado" className="encabezado">Tienda Virtual</h1>
+        <h1 className="encabezado">Tienda Virtual</h1>
     )
 }
 

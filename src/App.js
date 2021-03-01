88ec4d9a1b18847f29e5eddc34607846a35970a0
Armando <armandotechno12@gmail.com>
2021-03-01 18:43:40 -0400

    Primer uso del State de React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -6,6 +6,16 @@ import Footer from './components/Footer'
 
 function App() {
 
+  //crear listado de productos
+  const [ productos, guardarProductos ] = useState([
+    { id: 1, nombre: 'Camisa ReactJS', precio: 50 },
+    { id: 2, nombre: 'Camisa Blanca', precio: 60 },
+    { id: 3, nombre: 'Camisa Negra', precio: 30 },
+    { id: 4, nombre: 'Camisa Angular', precio: 20 }
+  ]);
+
+
+  //obtener fecha
   const fecha = new Date().getFullYear();
 
   return (
